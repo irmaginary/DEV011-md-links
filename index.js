61843b8215dd100ac5441ba95ecdee150f8506a2
IrmaSB <i.solano.baca@gmail.com>
2023-11-21 15:05:36 -0600

    todo bien hasta ruta absoluta. Hay que arreglar test

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+const path = require("path");
+
+const mdLinks = (inputPath, options) => {
+  return new Promise((resolve, reject) => {
+    // Identificar si la ruta existe
+    if (fs.existsSync(inputPath)) {
+      // Identificar si la ruta es absoluta
+      const esAbsoluta = path.isAbsolute(inputPath);
+
+      if (!esAbsoluta) {
+        // Si no es absoluta: convertir a absoluta
+        inputPath = path.resolve(inputPath);
+        resolve("Ruta convertida");
+      } else {
+        // Si es absoluta
+        resolve("Es absoluta");
+      }
+
+      // Aquí puedes agregar la lógica para verificar si es un archivo o directorio
+    } else {
+      // Si no existe la ruta: reject
+      reject("La ruta no existe");
+    }
+  });
 };
+
+module.exports = {
+  mdLinks,
+};
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,23 @@
-const mdLinks = require('../');
+const mdLinks = require("../index.js");
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("mdLinks", () => {
+  it("should...", () => {
+    console.log("FIX ME!");
   });
+  // it('should return a Promise', () => {
+  //   expect(mdLinks()).toBe(typeof Promise);
+  // });
+  it("should reject an unexistent path", () => {
+    return mdLinks("/cuso/laboratoria/noexiste.md").catch((error) => {
+      expect(error).toBe("ruta no encontrada");
+    });
+  });
+});
 
+describe("mdLinks", () => {
+  it("debe rechazar cuando la ruta no existe", () => {
+    return mdLinks("rutaFalsa/archivoFalso.md").catch((error) => {
+      expect(error).toBe("La ruta no existe");
+    });
+  });
 });
