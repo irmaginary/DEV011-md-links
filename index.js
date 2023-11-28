bc59bdf08cfa45ef1139294e2e0d95fe7cc14641
IrmaSB <i.solano.baca@gmail.com>
2023-11-27 20:55:32 -0600

    hito 1 listo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 // const axios = require("axios");
 // const { Remarkable } = require("remarkable");
 
-const mdLinks = (inputPath, options) => {
+const mdLinks = (inputPath, validate) => {
   return new Promise((resolve, reject) => {
     // Identificar si la ruta existe
     if (fs.existsSync(inputPath)) {
@@ -17,7 +17,6 @@ const mdLinks = (inputPath, options) => {
         // Si es absoluta
         console.log("Es absoluta");
       }
-
       // Verificar si es un archivo o directorio comprobando extensión de md
       const mdExtensiones = [
         ".md",
@@ -33,7 +32,7 @@ const mdLinks = (inputPath, options) => {
       if (mdExtensiones.includes(verifExtension)) {
         console.log("Es un archivo Markdown.");
 
-        // Leer y extraer enlaces utilizando remarkable
+        // Leer y extraer enlaces 
         fs.readFile(inputPath, "utf-8", (err, contenidoMarkdown) => {
           if (err) {
             console.error(`Error al leer el archivo: ${err.message}`);
@@ -44,61 +43,14 @@ const mdLinks = (inputPath, options) => {
             let match;
             while((match = regex.exec(contenidoMarkdown)) !== null) {
               links.push({
-                href: match[2],
-                text: match[1],
-                file: inputPath,
+                href: match[2], //extrae URL
+                text: match[1], //extrae tíulo
+                file: inputPath, //extrae ruta
               });
             }
             resolve(links);
           }
-        // const remarkable = new Remarkable();
-        // const tokens = remarkable.parse(contenidoMarkdown, {});
-
-        // // Filtrar los tokens para obtener solo los enlaces
-        // const urls = tokens
-        //   .filter((token) => token.type === "inline")
-        //   .map((token) => {
-        //     // Extraer URLs del contenido del token
-        //     const urlsInline = token.content.match(/\]\(([^)]+)\)/g);
-        //     if (urlsInline) {
-        //       return urlsInline.map((url) => url.substring(2, url.length - 1));
-        //     }
-        //     return [];
-        //   })
-        //   .flat();
-        // // Filtrar las URLs para excluir las que tienen #
-        // const urlsFiltradas = urls.filter((url) => !url.startsWith("#"));
-        // console.log("URLs de enlaces encontrados:");
-        // console.log(urlsFiltradas);
-        // resolve(urlsFiltradas);
       });
-
-        // Usar axios para realizar solicitudes HTTP con las URLs extraídas
-        // const solicitudesHTTP = urlsFiltradas.map((url) =>
-        //   axios
-        //     .get(url)
-        //     .then((response) => {
-        //       console.log(
-        //         `Solicitud a ${url} exitosa. Estado: ${response.status}`
-        //       );
-        //     })
-        //     .catch((error) => {
-        //       console.error(
-        //         `Error al realizar la solicitud a ${url}: ${error.message}`
-        //       );
-        //     })
-        // );
-
-        // // Esperar a que todas las solicitudes HTTP se completen antes de resolver la promesa
-        // Promise.all(solicitudesHTTP)
-        //   .then(() => {
-        //     console.log("Todas las solicitudes HTTP completadas.");
-        //     resolve(urlsFiltradas);
-        //   })
-        //   .catch((error) => {
-        //     console.error("Error al realizar las solicitudes HTTP:", error);
-        //     reject(error);
-        //   });
       } else {
         console.log("No es un archivo Markdown.");
         reject("No es un archivo Markdown.");
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,6 @@
 const { mdLinks } = require("../index.js");
 
+
 describe("mdLinks", () => {
   it("Debería ser una función", () => {
     console.log("FIX ME!");
@@ -8,6 +9,22 @@ describe("mdLinks", () => {
   //   expect(mdLinks()).toBe(typeof Promise);
   // });
 });
+// describe("verifExtension", () => {
+//   it("Debe rechazar cuando no es un archivo md", () => {
+//     return verifExtension("/archivoFalso.mg").catch((error) => {
+//       expect(error).toBe("No es un archivo Markdown.");
+//     });
+//   });
+// });
+describe("mdLinks", () => {
+  it("Debe rechazar cuando no es un archivo md", () => {
+    return mdLinks("/archivoFalso.mg").catch((error) => {
+      //Por la lógica de programación siempre rechaza un archivo falso antes de identificar si es o no md. Entonces, esta línea es para manejar ambos mensajes de error
+      expect(error).toMatch(/(No es un archivo Markdown.|La ruta no existe)/);
+    });
+  });
+});
+
 
 describe("mdLinks", () => {
   it("Debe rechazar cuando la ruta no existe", () => {
@@ -16,3 +33,25 @@ describe("mdLinks", () => {
     });
   });
 });
+
+describe("mdLinks", () => {
+  it("Debería resolver un arreglo con enlaces para un archivo .md", () => {
+    const filePath = "/Users/ru/Desktop/DEV011-md-links/DEV011-md-links/README.md";
+
+    return mdLinks(filePath).then((links) => {
+      expect(links).toEqual(expect.any(Array));
+      expect(links.length).toBeGreaterThan(0);
+
+      // El array contiene un enlace por elemento
+      links.forEach((link) => {
+        expect(link).toEqual(expect.objectContaining({
+          href: expect.any(String),
+          text: expect.any(String),
+          file: filePath,
+        }));
+      });
+    });
+  });
+});
+
+
